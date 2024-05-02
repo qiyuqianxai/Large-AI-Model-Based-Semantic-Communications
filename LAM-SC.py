06b7302fb03e7ebd55738aab17f0d76407cd4028
qiyuqianxai <2358341499@qq.com>
2024-05-02 08:43:03 +0800

    0502

--- LAM-SC.py
+++ LAM-SC.py
@@ -1,9 +1,20 @@
+import os
+
 import SKB
 import ASI
 import SC_with_ASC
 if __name__ == '__main__':
-    image_path = "images/1.png"
-    SKB.SKB_with_auto(image_path)
-    ASI.semantic_aware_images_generation(image_path.replace(".png",""))
-    SC_with_ASC.data_transmission(".")
+    # image segment based on SKB
+    raw_images_path = "raw_images" # path of raw images
+    for img in os.listdir(raw_images_path):
+        img_path = os.path.join(raw_images_path,img)
+        SKB.SKB_with_auto(img_path)
+    # semantic-aware image generation based on ASI
+    segment_images_path = "seg_images" # path of segment images
+    ASI.semantic_aware_images_generation(segment_images_path)
+
+    # image SC based on SC with ASC
+    sem_aware_images_path = "sa_images"
+    SC_with_ASC.data_transmission(sem_aware_images_path)
+
 
